ab92b84629d16cd0565b8be6856087eb89d31de9
micohrocode <michaelrocode@gmail.com>
2025-05-15 12:20:42 -0400

    divide and conquer 2

--- sorting_divide_and_conquer/main.py
+++ sorting_divide_and_conquer/main.py
@@ -1,89 +1,85 @@
-# TODO: find a video exaplanation of mergesort and quicksort
-def mergesort(l):
-    if len(l)<2:
-        return
+# mergesort:
+# split into equal parts
+# split until they cannot be split
+# sort them now
+# combine them
+def merge(arr, l, m, r):
+    n1 = m - 1 + 1
+    n2 = r - m
+
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    # Copy data to temp arrays L[] and R[]
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+
+    # Merge the temp arrays back into arr[l..r]
+    i = 0     # Initial index of first subarray
+    j = 0     # Initial index of second subarray
+    k = l     # Initial index of merged subarray
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
     
-    # get the two halves of the list
-    mid = len(l) // 2
-    a = l[:mid]
-    b = l[mid:]
+    # Copy the remaining elements of L[], if there
+    # are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
 
-    mergesort(a)
-    mergesort(b)
+    # Copy the remaining elements of R[], if there
+    # are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
 
-    # combine them
-    merge(a,b,l)
+def mergeSort(arr, l, r):
+    if l < r:
 
-def merge(a,b,l):
-    # index for a
-    i = 0 
-    # index for b
-    j = 0
+        # Same as (l+r)//2, but avoids overflow for
+        # large l and h
+        m = l+(r-l)//2
 
-    while i < len(a) and j < len(b):
-        # if the current a is smaller
-        if a[i] < b[i]:
-            # place the a number
-            l[i+j] = a[i]
-            # move the index
-            i+=1
-        else:
-            # place the b number
-            l[i+j] = b[j]
-            # move the index
-            j+=1
+        # Sort first and second halves
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+        merge(arr, l, m, r)
 
-    # place all remaining numbers
-    l[i+j:] = a[i:] + b[j:]
+# quick sort
+# choose a pivot
+# all smaller to left of pivot
+# larger to right of pivot
+# do this again for the left and right side
+# stops recursion when only one element left in subarray
 
-# iterators
-class SimpleIterator:
-    def __init__(self):
-        self.count = 0
+def partition(array, low, high):
+    # rightmost is pivot
+    pivot = array[high]
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.count <10:
-            self.count += 1
-            return self.count
-        else:
-            raise StopIteration
-
-def quicksort(l, left = 0, right = None):
-    if right is None:
-        right = len(l)
-
-    if right - left > 1:
-        # divide it
-        mid = partition(l, left, right)
-
-        quicksort(l, left, mid)
-        quicksort(l, mid+1, right)
-
-def partition(l, left, right):
-    pivot = right - 1
-    # left index
-    i = left
-    # right index
-    j = pivot - 1
-
-    while i < j:
-        # move i to point to something larger
-        while l[i] < l[pivot]:
+    # pointer for greater element
+    i = low - 1
+    for j in range(low,high):
+        if array[j] <= pivot:
             i = i + 1
-
-        # move j to point to something smaller
-        while i < j and l[j] >= l[pivot]:
-            j = j - 1
-
-        # swap the elements if i < j
-        if i<j:
-            l[i], l[j] = l[j], l[i]
+            (array[i], array[j]) = (array[j], array[i])
     
-    if l[pivot] <= l[i]:
-        l[pivot], l[i] = l[i], l[pivot]
-        pivot = i
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i+1
 
-    return pivot
+def quicksort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quicksort(array, low, pi-1)
+        quicksort(array, pi + 1, high)
